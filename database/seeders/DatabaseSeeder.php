099b3a7c054f4ee0af5dd222fbee5bce6fa9d0d4
katrinbeeva <106659227+katrinbeeva@users.noreply.github.com>
2023-01-13 10:35:34 +0200

    add AdminSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        //database seeder is looking for adminseeder
+        $this->call(AdminSeeder::class);
+        // this way we refresh _____ php artisan migrate:fresh --seed
     }
 }
